e547d3f674bea3cd7485b67782a221cebcff4b10
KennethChin0 <kchin00@stuy.edu>
2020-03-01 22:58:25 -0500

    allow display

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ screen = new_screen()
 color = [ 0, 255, 100 ]
 edges = []
 transform = new_matrix()
-# parse_file( 'script', edges, transform, screen, color )
-parse_file( 'myscript', edges, transform, screen, color )
+parse_file( 'script', edges, transform, screen, color )
+# parse_file( 'myscript', edges, transform, screen, color )
