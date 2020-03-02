2592676a68dff76e8c9487005f1eb4d6d0c3466c
KennethChin0 <kchin00@stuy.edu>
2020-03-01 22:55:20 -0500

    my image

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from draw import *
 from matrix import *
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
+color = [ 0, 255, 100 ]
 edges = []
 transform = new_matrix()
-parse_file( 'script', edges, transform, screen, color )
+# parse_file( 'script', edges, transform, screen, color )
+parse_file( 'myscript', edges, transform, screen, color )
